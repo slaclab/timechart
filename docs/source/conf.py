8c6fc01947bd3625970520d3e6f42b0fdd6b8de0
Hai Bui <hbui@slac.stanford.edu>
2019-01-14 22:39:44 -0800

    DOC: Use the TimeChart version as the documentation version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+import os
+
+module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../')
+sys.path.insert(0,module_path)
+
+import timechart
 
 
 # -- Project information -----------------------------------------------------
@@ -24,9 +28,9 @@ copyright = '2019, hbui'
 author = 'Hai Bui'
 
 # The short X.Y version
-version = ''
+version = timechart.__version__
 # The full version, including alpha/beta/rc tags
-release = '1.2.1'
+release = timechart.__version__
 
 
 # -- General configuration ---------------------------------------------------
