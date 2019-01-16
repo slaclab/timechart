a03befed86d94931ccba9d7f6d899454bacdcbd8
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-01-16 18:01:03 +0000

    Update docs after building Travis build 17 of slaclab/timechart
    The docs were built from the branch 'master' against the commit
    a0ce2cec01779d0fc8c9ebaa861a5ede26e80fcd.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/slaclab/timechart/jobs/480504696.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1+24.g8ce2d34.dirty',
+    VERSION: '1.2.1+27.ga0ce2ce.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
